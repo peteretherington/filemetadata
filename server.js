d30111b7563399a5154317ec329acc28802ea5d5
Peter Etherington <pjg.etherington@gmail.com>
2020-01-12 16:05:12 -0500

    Basic setup of multer

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 'use strict';
+
 require('dotenv').config()
-var express = require('express');
-var cors = require('cors');
-
-// require and use "multer"...
-
-var app = express();
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -14,8 +14,8 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.get('/api/test', function(req, res){
-  res.json({message: "Test endpoint working"});
+app.post('/api/fileanalyse', upload.single('fileUpload'), function(req, res){
+  res.json(req.file);
 });
 
 app.listen(process.env.PORT, function () {
